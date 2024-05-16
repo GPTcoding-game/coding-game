46bafd20bdd43a2c783c9d843c71ca48016dd926
Yoon Jun Pil <galaxy160102@gmail.com>
2024-05-16 15:57:21 +0900

    🍪feat : qna CRUD 작업 (user 관련 부분은 수정 해야함 )

--- src/main/java/com/jpms/codinggame/repository/QnaRepository.java
+++ src/main/java/com/jpms/codinggame/repository/QnaRepository.java
@@ -3,8 +3,9 @@ package com.jpms.codinggame.repository;
 import com.jpms.codinggame.entity.Qna;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface QnaRepository extends JpaRepository<Qna, Long> {
-    public Optional<Qna> findByQuestionId(Long question_id);
+    List<Qna> findAllByQuestionId(Long question_id);
 }
--- src/main/java/com/jpms/codinggame/service/QnaService.java
+++ src/main/java/com/jpms/codinggame/service/QnaService.java
@@ -1,13 +1,20 @@
 package com.jpms.codinggame.service;
 
 import com.jpms.codinggame.dto.QnaCreateRequestDto;
+import com.jpms.codinggame.dto.QnaModifyRequestDto;
+import com.jpms.codinggame.dto.QnaResponseDto;
 import com.jpms.codinggame.entity.Qna;
+import com.jpms.codinggame.entity.Question;
 import com.jpms.codinggame.repository.QnaRepository;
 import com.jpms.codinggame.repository.QuestionRepository;
 import com.jpms.codinggame.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -17,20 +24,70 @@ public class QnaService {
     private final QuestionRepository questionRepository;
 
     //생성
-    public void createQna(Long questionId,QnaCreateRequestDto dto){
-        //Question 존재 유무 판별
-        questionRepository.findById(questionId).orElseThrow(RuntimeException::new);
-
-
+    public void createQna(
+            Long questionId,
+            QnaCreateRequestDto dto){
+        //todo Qna 객체에 작성자(username)이 있어야 할 것 같은데, 이거는 Authentication 객체 안에 있는 username 을 끌어와야 할 것 같다
+        //일단 없이 진행하기
         qnaRepository.save(Qna
                         .builder()
                         .title(dto.getTitle())
                         .content(dto.getContent())
-//                        .question()
+                        .question(questionRepository.findById(questionId).orElseThrow(RuntimeException::new))
 //                        .user()
                         .time(dto.getTime())
                         .build());
     }
+
+
     //수정
+    public void modifyQna(
+            Long qnaId,
+            Long questionId,
+            QnaModifyRequestDto dto){
+        Qna qna = qnaRepository.findById(qnaId).orElseThrow(RuntimeException::new);
+        qnaRepository.save(Qna
+                .builder()
+                .id(qna.getId())
+                .title(dto.getTitle())
+                .content(dto.getContent())
+                .question(questionRepository.findById(questionId).orElseThrow(RuntimeException::new))
+//                .user()
+                .time(dto.getTime())
+                .build());
+    };
+
+
     //삭제
+    public void deleteQna(Long qnaId){
+        Qna qna = qnaRepository.findById(qnaId).orElseThrow(RuntimeException::new);
+        qnaRepository.deleteById(qnaId);
+    };
+
+
+    //질문 전체 가져오기
+    //page 형태로 변경하기
+    public List<QnaResponseDto> getQnaList(Long questionId){
+        //Question 존재 유무
+        questionRepository.findById(questionId).orElseThrow(RuntimeException::new);
+
+        //qnaList 생성
+        List<Qna> qnaList = qnaRepository.findAllByQuestionId(questionId);
+        return qnaList
+                .stream()
+                .map(qna -> QnaResponseDto
+                        .builder()
+                        .title(qna.getTitle())
+                        .content(qna.getContent())
+                        .time(qna.getTime())
+                        .build())
+                .collect(Collectors.toList());
+    }
+
+
+    //특정 질문 가져오기
+    public QnaResponseDto getQna(Long qnaId){
+        Qna qna = qnaRepository.findById(qnaId).orElseThrow(RuntimeException::new);
+        return QnaResponseDto.fromEntity(qna);
+    }
 }

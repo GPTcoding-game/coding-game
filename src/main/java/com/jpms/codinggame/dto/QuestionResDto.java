bb45a3aa2f8bf7ccedbab205e192d59b7eab8b8f
Yoon Jun Pil <galaxy160102@gmail.com>
2024-05-29 11:38:54 +0900

    🍪feature : 게임 로직 구현

---
+++ src/main/java/com/jpms/codinggame/dto/QuestionResDto.java
@@ -0,0 +1,13 @@
+package com.jpms.codinggame.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class QuestionResDto {
+    private int questionNo;
+    private String content;
+    private String choice;
+    private String answer;
+}

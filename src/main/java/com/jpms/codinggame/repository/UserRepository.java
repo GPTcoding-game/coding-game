79452dc2979a4171677888ca250fdb38541850f5
Yoon Jun Pil <galaxy160102@gmail.com>
2024-06-02 13:05:07 +0900

    🍪fix : user 상태 update 쿼리 수정

--- src/main/java/com/jpms/codinggame/repository/UserRepository.java
+++ src/main/java/com/jpms/codinggame/repository/UserRepository.java
@@ -1,8 +1,11 @@
 package com.jpms.codinggame.repository;
 
 import com.jpms.codinggame.entity.User;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -16,6 +19,8 @@ public interface UserRepository extends JpaRepository<User,Long> {
 
     List<User> findTop50ByOrderByScoreDesc();
 
-    @Query("UPDATE User u SET u.isDone = true")
-    void updateAllState();
+    @Modifying
+    @Query("UPDATE User u SET u.isDone = true WHERE u.id = :id")
+    @Transactional
+    void updateState(@Param("id") Long id);
 }

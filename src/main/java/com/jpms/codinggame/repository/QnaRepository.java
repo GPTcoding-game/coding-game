088d30d9f347805501a8de233e97bcdab8db19e0
Yoon Jun Pil <galaxy160102@gmail.com>
2024-06-04 12:25:54 +0900

    🍪fix : Qna dto 에 qnaId 필드값 추가 / 최근 qna 불러오기 기능 추가

--- src/main/java/com/jpms/codinggame/repository/QnaRepository.java
+++ src/main/java/com/jpms/codinggame/repository/QnaRepository.java
@@ -4,8 +4,8 @@ import com.jpms.codinggame.entity.Qna;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface QnaRepository extends JpaRepository<Qna, Long> {
     List<Qna> findAllByQuestionId(Long question_id);
+    List<Qna> findTop5ByOrderByIdDesc();
 }
